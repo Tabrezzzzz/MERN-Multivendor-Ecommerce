e983a233f975cc944aa92ca8f97109791eecbf13
zdbolt12 <102662896+zdbolt12@users.noreply.github.com>
2022-12-07 21:23:48 +0530

    Create vendorModel.js

---
+++ Models/vendorModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const vendorRegisterSchema = mongoose.Schema({
+    companyname:{
+        type: String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+
+    },
+    userid:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+},{timestamp: true}
+);
+
+const vendorModel = mongoose.model('vendor', vendorRegisterSchema);
+
+module.exports = vendorModel;
