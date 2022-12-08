74af029f2b394fbcbb177f67381423dcf54c752e
Tabrez Malim <tabrezmalim01@gmail.com>
2022-12-08 21:47:08 +0530

    created login validation and forgot password

--- Routes/customerRoutes.js
+++ Routes/customerRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const {
     registerCustomerController,
-    loginCustomerController
+    loginCustomerController,
+    forgotPasswordController
 } = require('../Controllers/customerControllers')
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 
 router.post("/customer-register", registerCustomerController);
 router.post("/customer-login", loginCustomerController);
+router.post("/forgot-password", forgotPasswordController);
 
 module.exports = router;
