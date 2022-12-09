69116cbfd4a5f7ec664d8e5847ef7838ddcbf1be
Tabrez Malim <tabrezmalim01@gmail.com>
2022-12-09 21:30:02 +0530

    login form api connected

--- client/src/pages/LoginRegister.js
+++ client/src/pages/LoginRegister.js
@@ -11,7 +11,29 @@ import {
     Box,
     Text
   } from '@chakra-ui/react'
+  import { useFormik } from 'formik';
+import axios from '../API Server/axios';
+
+
+
+
   export const LoginRegister = () => {
+
+    const formik = useFormik({
+      initialValues: {
+        username: '',
+        password:''
+      },
+      onSubmit: async(values) => {
+        try{
+          const res = await axios.post("/api/customer/customer-login", values)
+          console.log(res)
+        }catch(error){
+          console.log(error)
+        }
+        
+      },
+    });
 
     const handleLogin = (e) => {
       e.preventDefault()
@@ -23,12 +45,12 @@ import {
     <Box>
   <Box width={"500px"} boxShadow="2px 2px 10px #00000042" padding={"20px"} mt="150px" ms="150px" borderRadius={"10px"} bgImage="radial-gradient(#fecd21, #f47412)" color={"white !important"}>
   <Text mb="20px" fontSize={"30px"} fontWeight={500} textAlign={"center"}>LOGIN</Text>
-  <form onSubmit={(e) => handleLogin(e)}>
+  <form onSubmit={formik.handleSubmit}>
   <FormControl >
-  <FormLabel>Email Address</FormLabel>
-  <Input type='email' />
+  <FormLabel>Username</FormLabel>
+  <Input name='username' type='text' onChange={formik.handleChange} value={formik.values.username} />
   <FormLabel>Password</FormLabel>
-  <Input type='password' />
+  <Input name='password' onChange={formik.handleChange} value={formik.values.password} type='password' />
   <Input type={"submit"} value="LOGIN" mt={"20px"} cursor="pointer"/>
 </FormControl>
 </form>
